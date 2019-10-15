eb3311724f3d6027b61659d4bb54a56340f81be4
William Noris <w.noris@protonmail.com>
2019-10-16 00:37:54 +0200

    gestion du bouton de depart et d'arret

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,6 +1,8 @@
 var lastClicked;
 var taille = 20; // on décide que la grille sera toujours un carré
+var nbImagesSecs = 10;
 var jeu = matrice(taille);
+console.log(jeu);
 var grid = clickableGrid(taille,function(el,row,col){
     console.log("You clicked on element:",el);
     console.log("You clicked on row:",row);
@@ -18,6 +20,8 @@ var grid = clickableGrid(taille,function(el,row,col){
 });
 
 document.body.appendChild(grid);
+
+document.getElementById("theButton").addEventListener("click", demarrer);
 
 //construction de la grille de depart (tut est blanc)
 function clickableGrid(_taille, callback ){
@@ -44,10 +48,43 @@ function clickableGrid(_taille, callback ){
 function matrice(_taille){
   //Initialisation de la matrice de base, où tout est à 0 (blanc, mort)
   var matrix = [];
-  for( var r = 0; r < _taille; r++){
+  for(var r = 0; r < _taille; r++){
+    var column = [];
     for(var c = 0; c < _taille; c++){
-      matrix[r, c] = 0;
+      //console.log(`r : ${r}, c : ${c}`);
+      column[c] = 0;
+      //matrix[r][c] = 0;
     }
+    matrix[r] = column;
   }
   return matrix;
+}
+
+
+var intervalID;
+function demarrer(){
+  var theButton = document.getElementById("theButton");
+  theButton.value = "stopper";
+  theButton.removeEventListener("click", demarrer);
+  theButton.addEventListener("click", stopper);
+
+  //TODO: lancer la simulation
+  intervalID = window.setInterval(iterationSimu, 1000/nbImagesSecs);
+
+}
+
+function stopper(){
+  var theButton = document.getElementById("theButton");
+  theButton.value = "démarrer";
+  theButton.removeEventListener("click", stopper);
+  theButton.addEventListener("click", demarrer);
+
+  //TODO: arrêter la simulation
+  window.clearInterval(intervalID);
+
+}
+
+function iterationSimu(){
+  //TODO: calculer chaque itération du jeu
+  console.log("Itération du jeu !");
 }
